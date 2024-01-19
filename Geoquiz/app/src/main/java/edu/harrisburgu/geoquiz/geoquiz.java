81d001755e9cf164c5d73e3ce50916f04b813152
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2024-01-19 11:49:09 -0500

    geoquiz initial

---
+++ Geoquiz/app/src/main/java/edu/harrisburgu/geoquiz/geoquiz.java
@@ -0,0 +1,35 @@
+package edu.harrisburgu.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class geoquiz extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(geoquiz.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(geoquiz.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
