018d13e82cfa0c9d8f18429016be6f9568539366
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2024-03-29 09:23:04 -0400

    adding DB to cameratest

--- cameraTest/app/src/main/java/edu/harrisburgu/jasonb75/cameratest/MainActivity.java
+++ cameraTest/app/src/main/java/edu/harrisburgu/jasonb75/cameratest/MainActivity.java
@@ -8,10 +8,16 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE){
-            if (resultCode == RESULT_OK){
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
                 assert data != null;
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
@@ -50,9 +56,50 @@ public class MainActivity extends AppCompatActivity {
 
                 drawable = (BitmapDrawable) imageView.getDrawable();
                 final Bitmap bitmap = drawable.getBitmap();
-            } else if (resultCode == RESULT_CANCELED){
+
+                uploadToServer(encodeToBase64(bitmap, Bitmap.CompressFormat.PNG, 100));
+
+            } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "You cancelled operation", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public static String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality) {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+
+    private void uploadToServer(final String image) {
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JSONObject json = new JSONObject();
+        try {
+            json.put("store", "amz");
+            json.put("image", image);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = "http://10.1.121.45:5000/image";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Hello", "Response: " + response.toString());
+                        Logger.d("hello");
+                        Logger.json(String.valueOf(response));
+                        Logger.e("error");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Hello", error.getMessage());
+            }
+        });
+
+        queue.add(jsonObjectRequest);
     }
 }
