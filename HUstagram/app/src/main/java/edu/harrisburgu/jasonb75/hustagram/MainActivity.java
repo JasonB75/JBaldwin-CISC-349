db5b76c5cf1b1d6e60cd1020f3065fca6e6dc4f9
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2024-03-29 11:56:21 -0400

    HUstigram main screen and upload

--- HUstagram/app/src/main/java/edu/harrisburgu/jasonb75/hustagram/MainActivity.java
+++ HUstagram/app/src/main/java/edu/harrisburgu/jasonb75/hustagram/MainActivity.java
@@ -1,4 +1,4 @@
-package edu.harrisburgu.jasonb75.cameratest;
+package edu.harrisburgu.jasonb75.hustagram;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +13,7 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,39 +22,53 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    ImageView imageView;
-    BitmapDrawable drawable;
-    Button captureButton;
+    private ImageView imageView;
+
+    private Button cameraButton, uploadButton, viewImagesButton;
+    private EditText commentEditText;
+
     private Context context;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private BitmapDrawable drawable;
 
-    Calendar calendar = new GregorianCalendar();
+    protected RequestQueue queue;
 
+    protected String  uploadURL = "http://10.2.98.125:5000/save_image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        captureButton = findViewById(R.id.camera_button);
-        imageView = findViewById(R.id.cameraImageView);
         context = this;
 
-        captureButton.setOnClickListener(new View.OnClickListener() {
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        //Buttons
+        cameraButton = findViewById(R.id.takePhotoButton);
+        uploadButton = findViewById(R.id.uploadButton);
+        viewImagesButton = findViewById(R.id.viewImagesButton);
+
+        //Comment Edit text
+        commentEditText = findViewById(R.id.commentEditText);
+
+        //Main image view on the main screen, to view the picture just taken
+        imageView = findViewById(R.id.mainImageView);
+
+        //When the campture image button is pressed, send an intent to the built in image capture
+        cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -61,40 +76,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button listButton = findViewById(R.id.list_button);
-        listButton.setOnClickListener(new View.OnClickListener() {
+        //When the upload button is clocked, start the upload
+        uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Camera Test", "List button clock");
-                Intent i = new Intent(MainActivity.this, ImageListView.class);
-                startActivity(i);
+                drawable = (BitmapDrawable) imageView.getDrawable();
+                final Bitmap bitmap = drawable.getBitmap();
+
+                String comments = commentEditText.getText().toString();
+                if (comments.length() < 1) comments = " ";
+
+                uploadToServer(encodeToBase64(bitmap, Bitmap.CompressFormat.PNG, 100),
+                        comments,
+                        getDT(),
+                        queue);
             }
         });
 
-        SimpleDateFormat sdf = new SimpleDateFormat("'Date 'dd-MM-yyyy 'and Time 'HH:mm:ss z");
-        String currentDateAndTime = sdf.format(new Date());
 
-
-        Log.d("CALANDER TEST: ", currentDateAndTime);
     }
 
-    @Override
+
+    @Override // This handles the return from the image capture activity
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == REQUEST_IMAGE_CAPTURE) {
             if (resultCode == RESULT_OK) {
                 assert data != null;
+
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 imageView.setImageBitmap(imageBitmap);
 
-                drawable = (BitmapDrawable) imageView.getDrawable();
-                final Bitmap bitmap = drawable.getBitmap();
-
-                uploadToServer(encodeToBase64(bitmap, Bitmap.CompressFormat.PNG, 100));
-
             } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "You cancelled operation", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Picture Cancelled", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -106,24 +122,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void uploadToServer(final String image) {
-
-        RequestQueue queue = Volley.newRequestQueue(context);
+    private void uploadToServer(final String image, String comment, String dateAndTime, RequestQueue queue) {
         JSONObject json = new JSONObject();
         try {
-            json.put("store", "amz");
+            json.put("comment", comment);
+            json.put("dateTime", dateAndTime);
             json.put("image", image);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        String url = "http://10.2.98.125:5000/image";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, uploadURL, json,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d("Hello", "Response: " + response.toString());
-
+                        try {
+                            Log.d("Date Time: ", json.getString("dateTime"));
+                            Log.d("comment: ", json.getString("comment"));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -134,4 +153,16 @@ public class MainActivity extends AppCompatActivity {
 
         queue.add(jsonObjectRequest);
     }
+
+    //A method to get the formated date and time in a string
+    private String getDT(){
+        SimpleDateFormat sdf = new SimpleDateFormat("'Date 'dd-MM-yyyy 'and Time 'HH:mm:ss z");
+        String currentDateAndTime = sdf.format(new Date());
+        return currentDateAndTime;
+    }
+
+
+
+
+
 }
---
+++ HUstagram/app/src/main/python/server.py
@@ -0,0 +1,47 @@
+import json
+from matplotlib import collections
+#from app import app
+from pymongo import MongoClient  
+from flask import request
+from flask.json import jsonify
+import sys
+import base64
+from datetime import datetime
+from flask import Flask
+
+client = MongoClient("mongodb+srv://jbaldwin:CBAE4VO2IAOtZjI9@cisc349.ni0blpr.mongodb.net/?retryWrites=true&w=majority&appName=CISC349")
+db = client["cisc349"]
+ 
+app = Flask(__name__)
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server 123 !!</h1>"
+
+
+@app.route("/images", methods=["GET"])
+def image_list():
+    collection = db["image"]
+    images = list(collection.find())
+    print(f"Got {len(images)} images.")
+    return json.dumps(images, default=str)
+
+
+@app.route("/save_image", methods=["POST"])
+def image_save():
+    collection = db["image"]
+    content = request.get_json()
+    image = content["image"]
+    comment = content["comment"]
+    dt = content["dateTime"]
+    data = {"image": image, "comment" : comment, "dateTime": dt}
+    _id = collection.insert_one(data)
+    return json.dumps({'id': str(_id.inserted_id)})
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    app.run("0.0.0.0",port=5000)
