284da5ce9fc36dd08ac5a83acb675528d76dbdfb
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2024-04-11 19:57:54 -0400

    changes

--- JournalApp/app/src/main/java/edu/harrisburgu/jasonb75/journalapp/HealthCollectionActivity.java
+++ JournalApp/app/src/main/java/edu/harrisburgu/jasonb75/journalapp/HealthCollectionActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class HealthCollectionActivity extends AppCompatActivity {
 
     private static JournalEntry journalEntry;
+
+    private SeekBar energySeekbar;
+
+    private TextView energyTextview;
 
 
     public static Intent newIntent(Context packageContext, String date, String time, int mood) {
@@ -21,5 +29,31 @@ public class HealthCollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_collection);
+
+        energyTextview = (TextView) findViewById(R.id.energyTextView);
+
+        energySeekbar = (SeekBar) findViewById(R.id.energySeekbar);
+
+        energySeekbar.setMax(10);
+        //energySeekbar.setMin(1);
+
+        energySeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                energyTextview.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("Energybar", "starttracking");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("Energybar", "endtracking");
+            }
+        });
+
+
     }
 }
