b178f509436611b61a25a5f97f356b3d76d072b6
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2024-04-12 11:05:23 -0400

    Made seekbar progressListeners into a function

--- JournalApp2/app/src/main/java/edu/harrisburgu/jasonb75/journalapp2/HealthCollectionActivity.java
+++ JournalApp2/app/src/main/java/edu/harrisburgu/jasonb75/journalapp2/HealthCollectionActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -47,12 +48,12 @@ public class HealthCollectionActivity extends AppCompatActivity {
 
 
         //Setting min and max for each seekbar
-        energySeekbar.setMax(10); energySeekbar.setMin(1);
-        sAmountSeekbar.setMax(24); sAmountSeekbar.setMin(0);
-        sQualitySeekbar.setMax(10); sQualitySeekbar.setMin(1);
-        socialBatterySeekbar.setMax(10); socialBatterySeekbar.setMin(1);
-        stomachFeelingSeekbar.setMax(10); stomachFeelingSeekbar.setMin(1);
-        eatSeekBar.setMax(24); eatSeekBar.setMin(0);
+        //energySeekbar.setMax(10); energySeekbar.setMin(1);
+        //sAmountSeekbar.setMax(24); sAmountSeekbar.setMin(0);
+        //sQualitySeekbar.setMax(10); sQualitySeekbar.setMin(1);
+        //socialBatterySeekbar.setMax(10); socialBatterySeekbar.setMin(1);
+        //stomachFeelingSeekbar.setMax(10); stomachFeelingSeekbar.setMin(1);
+        //eatSeekBar.setMax(24); eatSeekBar.setMin(0);
 
         //Setting the initial displayed values to the defaults of the seekbars
         energyTextview.setText(String.valueOf(energySeekbar.getProgress()));
@@ -60,83 +61,34 @@ public class HealthCollectionActivity extends AppCompatActivity {
         sQualityTextview.setText(String.valueOf(sQualitySeekbar.getProgress()));
         socialBatteryTextview.setText(String.valueOf(socialBatterySeekbar.getProgress()));
         stomachFeelingTextview.setText(String.valueOf(stomachFeelingSeekbar.getProgress()));
-        eatTextView.setText(String.valueOf(eatSeekBar.getProgress()));
+        //eatTextView.setText(String.valueOf(eatSeekBar.getProgress()));
 
+        seekBarSetup(energySeekbar, energyTextview, 1, 10);
+        seekBarSetup(sAmountSeekbar, sAmountTextview, 0, 24);
+        seekBarSetup(sQualitySeekbar, sQualityTextview, 1, 10);
+        seekBarSetup(socialBatterySeekbar, socialBatteryTextview, 1, 10);
+        seekBarSetup(stomachFeelingSeekbar, stomachFeelingTextview, 1, 10);
+        seekBarSetup(eatSeekBar, eatTextView, 0, 24);
+
+    }
+
+
+    private void seekBarSetup(SeekBar seekBar, TextView textView, int min, int max){
+        seekBar.setMin(min);
+        seekBar.setMax(max);
+        textView.setText(String.valueOf(seekBar.getProgress()));
 
         //When the seekbar is changed, change the textview value for the attribute
-        energySeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                energyTextview.setText(String.valueOf(progress));
+                textView.setText(String.valueOf(progress));
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
-
-        //When the seekbar is changed, change the textview value for the attribute
-        sAmountSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                sAmountTextview.setText(String.valueOf(progress));
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
-        //When the seekbar is changed, change the textview value for the attribute
-        sQualitySeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                sQualityTextview.setText(String.valueOf(progress));
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
-        //When the seekbar is changed, change the textview value for the attribute
-        socialBatterySeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                socialBatteryTextview.setText(String.valueOf(progress));
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
-        //When the seekbar is changed, change the textview value for the attribute
-        stomachFeelingSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                stomachFeelingTextview.setText(String.valueOf(progress));
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
-        //When the seekbar is changed, change the textview value for the attribute
-        eatSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                eatTextView.setText(String.valueOf(progress));
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
-
-
 
     }
 }
